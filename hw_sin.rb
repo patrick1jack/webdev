b75f615aa3804609c8ff487cea8f2a774de44bff
Patrick Jackson <triopat@Patrick-Jacksons-MacBook-Pro-9.local>
2014-10-09 15:38:19 -0700

    Initial Commit 2

--- hw_sin.rb
+++ hw_sin.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
-require 'sinatra'
-#require 'bundler/setup'
+#require 'sinatra'
+require 'bundler/setup'
 require 'json'
+Bundler.require
 
 file = File.read('hw_list.txt')
 
@@ -14,19 +15,19 @@ task, date = line.split("-")
   puts" #{task}: #{date}"
 end
 
-get '/form' do
+get '/' do
 file = File.read('hw_list.txt')
 @lines = file.split("\n")
 erb :sina
 end
 
-post '/form' do
+post '/' do
   
      File.open("hw_list.txt", "a") do |file|
      file.puts "#{params[:task]} - #{params[:date]}"
      
 end
-redirect '/form'
+redirect '/'
 end
 
 
