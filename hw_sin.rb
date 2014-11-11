8a134070da0b3ac29cdfc880148d78d23747e301
Patrick Jackson <triopat@Patrick-Jacksons-MacBook-Pro-9.local>
2014-11-10 19:38:03 -0800

    Set up delete

--- hw_sin.rb
+++ hw_sin.rb
@@ -25,7 +25,7 @@ post '/' do
      TodoItem.create(description: params[:task], due: params[:date])
 redirect '/'
 end
-delete '/delete/:id' do 
+post '/delete/:id' do 
        TodoItem.find(params[:id]).destroy
     
 redirect '/'
